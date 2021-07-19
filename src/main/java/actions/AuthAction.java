89960419218339f333dacb4edc0f56a7f8c53813
Ryosuke Aono <ryosuke_aono@r.recruit.co.jp>
2021-07-19 17:46:32 +0900

    ログインフィルタの作成

--- src/main/java/actions/AuthAction.java
+++ src/main/java/actions/AuthAction.java
@@ -11,71 +11,85 @@ import constants.MessageConst;
 import constants.PropertyConst;
 import services.EmployeeService;
 
-public class AuthAction extends ActionBase{
+public class AuthAction extends ActionBase {
 	private EmployeeService service;
 
 	@Override
-    public void process() throws ServletException, IOException {
+	public void process() throws ServletException, IOException {
 
-        service = new EmployeeService();
+		service = new EmployeeService();
 
-        //メソッドを実行
-        invoke();
+		//メソッドを実行
+		invoke();
 
-        service.close();
-    }
+		service.close();
+	}
+
 	public void showLogin() throws ServletException, IOException {
 
-        //CSRF対策用トークンを設定
-        putRequestScope(AttributeConst.TOKEN, getTokenId());
+		//CSRF対策用トークンを設定
+		putRequestScope(AttributeConst.TOKEN, getTokenId());
 
-        //セッションにフラッシュメッセージが登録されている場合はリクエストスコープに設定する
-        String flush = getSessionScope(AttributeConst.FLUSH);
-        if (flush != null) {
-            putRequestScope(
-                    AttributeConst.FLUSH,
-                    getSessionScope(AttributeConst.FLUSH));
-            removeSessionScope(AttributeConst.FLUSH);
-        }
+		//セッションにフラッシュメッセージが登録されている場合はリクエストスコープに設定する
+		String flush = getSessionScope(AttributeConst.FLUSH);
+		if (flush != null) {
+			putRequestScope(
+					AttributeConst.FLUSH,
+					getSessionScope(AttributeConst.FLUSH));
+			removeSessionScope(AttributeConst.FLUSH);
+		}
 
-        //ログイン画面を表示
-        forward(ForwardConst.FW_LOGIN);
-    }
+		//ログイン画面を表示
+		forward(ForwardConst.FW_LOGIN);
+	}
 
-	public void login() throws ServletException, IOException{
+	public void login() throws ServletException, IOException {
 		String code = getRequestParam(AttributeConst.EMP_CODE);
 		String plainPass = getRequestParam(AttributeConst.EMP_PASS);
-	    String pepper = getContextScope(PropertyConst.PEPPER);
+		String pepper = getContextScope(PropertyConst.PEPPER);
 
-	    Boolean isValidEmployee = service.validateLogin(code, plainPass, pepper);
+		Boolean isValidEmployee = service.validateLogin(code, plainPass, pepper);
 
-	    if (isValidEmployee) {
-	        //認証成功の場合
+		if (isValidEmployee) {
+			//認証成功の場合
 
-	        //CSRF対策 tokenのチェック
-	        if (checkToken()) {
+			//CSRF対策 tokenのチェック
+			if (checkToken()) {
 
-	            //ログインした従業員のDBデータを取得
-	            EmployeeView ev = service.findOne(code, plainPass, pepper);
-	            //セッションにログインした従業員を設定
-	            putSessionScope(AttributeConst.LOGIN_EMP, ev);
-	            //セッションにログイン完了のフラッシュメッセージを設定
-	            putSessionScope(AttributeConst.FLUSH, MessageConst.I_LOGINED.getMessage());
-	            //トップページへリダイレクト
-	            redirect(ForwardConst.ACT_TOP, ForwardConst.CMD_INDEX);
-	        }
-	    } else {
-	        //認証失敗の場合
+				//ログインした従業員のDBデータを取得
+				EmployeeView ev = service.findOne(code, plainPass, pepper);
+				//セッションにログインした従業員を設定
+				putSessionScope(AttributeConst.LOGIN_EMP, ev);
+				//セッションにログイン完了のフラッシュメッセージを設定
+				putSessionScope(AttributeConst.FLUSH, MessageConst.I_LOGINED.getMessage());
+				//トップページへリダイレクト
+				redirect(ForwardConst.ACT_TOP, ForwardConst.CMD_INDEX);
+			}
+		} else {
+			//認証失敗の場合
 
-	        //CSRF対策用トークンを設定
-	        putRequestScope(AttributeConst.TOKEN, getTokenId());
-	        //認証失敗エラーメッセージ表示フラグをたてる
-	        putRequestScope(AttributeConst.LOGIN_ERR, true);
-	        //入力された従業員コードを設定
-	        putRequestScope(AttributeConst.EMP_CODE, code);
+			//CSRF対策用トークンを設定
+			putRequestScope(AttributeConst.TOKEN, getTokenId());
+			//認証失敗エラーメッセージ表示フラグをたてる
+			putRequestScope(AttributeConst.LOGIN_ERR, true);
+			//入力された従業員コードを設定
+			putRequestScope(AttributeConst.EMP_CODE, code);
 
-	        //ログイン画面を表示
-	        forward(ForwardConst.FW_LOGIN);
-	    }
+			//ログイン画面を表示
+			forward(ForwardConst.FW_LOGIN);
+		}
+	}
+
+	public void logout() throws ServletException, IOException {
+
+		//セッションからログイン従業員のパラメータを削除
+		removeSessionScope(AttributeConst.LOGIN_EMP);
+
+		//セッションにログアウト時のフラッシュメッセージを追加
+		putSessionScope(AttributeConst.FLUSH, MessageConst.I_LOGOUT.getMessage());
+
+		//ログイン画面にリダイレクト
+		redirect(ForwardConst.ACT_AUTH, ForwardConst.CMD_SHOW_LOGIN);
+
 	}
 }
